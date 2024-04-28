63f5a1ca7b4cf49e3af4c4c55a49374d8bf25d57
zbmovo <zbmovo@foxmail.com>
2024-04-29 00:58:51 +0800

    chore: init working environment - installed eslint - installed prettier - installed husky - installed lint-staged - installed commitlint

---
+++ packages/midi/main.ts
@@ -0,0 +1,11 @@
+export function main(number: number) {
+  const string = "string"
+  switch (number) {
+    case 1: {
+      const result = 111
+      return result
+    }
+    case 2:
+      return string
+  }
+}
